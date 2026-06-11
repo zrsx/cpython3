30afb832d52881fbb7e7f1f265704c7b2f1fba70
George <201302550+Aasyaco@users.noreply.github.com>
2026-06-11 15:11:31 +0600

    Python 3.14 config updates

--- android/darwin/config.c
+++ android/darwin/config.c
@@ -1,14 +1,4 @@
 /* Generated automatically from Modules/config.c.in by makesetup. */
-/* -*- C -*- ***********************************************
-Copyright (c) 2000, BeOpen.com.
-Copyright (c) 1995-2000, Corporation for National Research Initiatives.
-Copyright (c) 1990-1995, Stichting Mathematisch Centrum.
-All rights reserved.
-
-See the file "Misc/COPYRIGHT" for information on usage and
-redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
-******************************************************************/
-
 /* Module configuration */
 
 /* !!! !!! !!! This file is edited by the makesetup script !!! !!! !!! */
@@ -28,15 +18,14 @@ extern PyObject* PyInit__hashlib(void);
 extern PyObject* PyInit_array(void);
 extern PyObject* PyInit__asyncio(void);
 extern PyObject* PyInit__bisect(void);
-extern PyObject* PyInit__contextvars(void);
 extern PyObject* PyInit__csv(void);
 extern PyObject* PyInit__heapq(void);
 extern PyObject* PyInit__json(void);
 extern PyObject* PyInit__lsprof(void);
-extern PyObject* PyInit__opcode(void);
 extern PyObject* PyInit__pickle(void);
 extern PyObject* PyInit__queue(void);
 extern PyObject* PyInit__random(void);
+extern PyObject* PyInit__remote_debugging(void);
 extern PyObject* PyInit__struct(void);
 extern PyObject* PyInit__interpreters(void);
 extern PyObject* PyInit__interpchannels(void);
@@ -45,16 +34,18 @@ extern PyObject* PyInit__zoneinfo(void);
 extern PyObject* PyInit_math(void);
 extern PyObject* PyInit_cmath(void);
 extern PyObject* PyInit__statistics(void);
-extern PyObject* PyInit__datetime(void);
 extern PyObject* PyInit__decimal(void);
 extern PyObject* PyInit_binascii(void);
 extern PyObject* PyInit__bz2(void);
+extern PyObject* PyInit__lzma(void);
+extern PyObject* PyInit__zstd(void);
 extern PyObject* PyInit_zlib(void);
 extern PyObject* PyInit__md5(void);
 extern PyObject* PyInit__sha1(void);
 extern PyObject* PyInit__sha2(void);
 extern PyObject* PyInit__sha3(void);
 extern PyObject* PyInit__blake2(void);
+extern PyObject* PyInit__hmac(void);
 extern PyObject* PyInit_pyexpat(void);
 extern PyObject* PyInit__elementtree(void);
 extern PyObject* PyInit__codecs_cn(void);
@@ -86,6 +77,7 @@ extern PyObject* PyInit_posix(void);
 extern PyObject* PyInit__signal(void);
 extern PyObject* PyInit__tracemalloc(void);
 extern PyObject* PyInit__suggestions(void);
+extern PyObject* PyInit__datetime(void);
 extern PyObject* PyInit__codecs(void);
 extern PyObject* PyInit__collections(void);
 extern PyObject* PyInit_errno(void);
@@ -95,11 +87,13 @@ extern PyObject* PyInit__sre(void);
 extern PyObject* PyInit__sysconfig(void);
 extern PyObject* PyInit__thread(void);
 extern PyObject* PyInit_time(void);
+extern PyObject* PyInit__types(void);
 extern PyObject* PyInit__typing(void);
 extern PyObject* PyInit__weakref(void);
 extern PyObject* PyInit__abc(void);
 extern PyObject* PyInit__functools(void);
 extern PyObject* PyInit__locale(void);
+extern PyObject* PyInit__opcode(void);
 extern PyObject* PyInit__operator(void);
 extern PyObject* PyInit__stat(void);
 extern PyObject* PyInit__symtable(void);
@@ -112,6 +106,7 @@ extern PyObject* PyInit__imp(void);
 extern PyObject* PyInit_gc(void);
 extern PyObject* PyInit__ast(void);
 extern PyObject* PyInit__tokenize(void);
+extern PyObject* PyInit__contextvars(void);
 extern PyObject* _PyWarnings_Init(void);
 extern PyObject* PyInit__string(void);
 
@@ -122,15 +117,14 @@ struct _inittab _PyImport_Inittab[] = {
     {"array", PyInit_array},
     {"_asyncio", PyInit__asyncio},
     {"_bisect", PyInit__bisect},
-    {"_contextvars", PyInit__contextvars},
     {"_csv", PyInit__csv},
     {"_heapq", PyInit__heapq},
     {"_json", PyInit__json},
     {"_lsprof", PyInit__lsprof},
-    {"_opcode", PyInit__opcode},
     {"_pickle", PyInit__pickle},
     {"_queue", PyInit__queue},
     {"_random", PyInit__random},
+    {"_remote_debugging", PyInit__remote_debugging},
     {"_struct", PyInit__struct},
     {"_interpreters", PyInit__interpreters},
     {"_interpchannels", PyInit__interpchannels},
@@ -139,16 +133,18 @@ struct _inittab _PyImport_Inittab[] = {
     {"math", PyInit_math},
     {"cmath", PyInit_cmath},
     {"_statistics", PyInit__statistics},
-    {"_datetime", PyInit__datetime},
     {"_decimal", PyInit__decimal},
     {"binascii", PyInit_binascii},
     {"_bz2", PyInit__bz2},
+    {"_lzma", PyInit__lzma},
+    {"_zstd", PyInit__zstd},
     {"zlib", PyInit_zlib},
     {"_md5", PyInit__md5},
     {"_sha1", PyInit__sha1},
     {"_sha2", PyInit__sha2},
     {"_sha3", PyInit__sha3},
     {"_blake2", PyInit__blake2},
+    {"_hmac", PyInit__hmac},
     {"pyexpat", PyInit_pyexpat},
     {"_elementtree", PyInit__elementtree},
     {"_codecs_cn", PyInit__codecs_cn},
@@ -180,6 +176,7 @@ struct _inittab _PyImport_Inittab[] = {
     {"_signal", PyInit__signal},
     {"_tracemalloc", PyInit__tracemalloc},
     {"_suggestions", PyInit__suggestions},
+    {"_datetime", PyInit__datetime},
     {"_codecs", PyInit__codecs},
     {"_collections", PyInit__collections},
     {"errno", PyInit_errno},
@@ -189,11 +186,13 @@ struct _inittab _PyImport_Inittab[] = {
     {"_sysconfig", PyInit__sysconfig},
     {"_thread", PyInit__thread},
     {"time", PyInit_time},
+    {"_types", PyInit__types},
     {"_typing", PyInit__typing},
     {"_weakref", PyInit__weakref},
     {"_abc", PyInit__abc},
     {"_functools", PyInit__functools},
     {"_locale", PyInit__locale},
+    {"_opcode", PyInit__opcode},
     {"_operator", PyInit__operator},
     {"_stat", PyInit__stat},
     {"_symtable", PyInit__symtable},
@@ -219,6 +218,9 @@ struct _inittab _PyImport_Inittab[] = {
 
     /* This lives in gcmodule.c */
     {"gc", PyInit_gc},
+
+    /* This lives in Python/_contextvars.c */
+    {"_contextvars", PyInit__contextvars},
 
     /* This lives in _warnings.c */
     {"_warnings", _PyWarnings_Init},
